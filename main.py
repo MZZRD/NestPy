a4cabed0e94606b548ba3bb1b1c4e82f8610861b
MZZRD <m.tenvoorde1998@gmail.com>
2024-04-23 11:24:13 +0200

    Implemented automatic refreshing of access token

--- main.py
+++ main.py
@@ -1,95 +1,139 @@
 import os
-from dotenv import load_dotenv, set_key
+from dotenv import find_dotenv, load_dotenv, set_key
 import click
 import requests
 
 
 class Nest(object):
-    def __init__(self, project_id, device_id, access_token):
-        # API variables
-        self.project_id = project_id
-        self.device_id = device_id
-        self.access_token = access_token
-
+    def __init__(
+        self,
+        project_id: str,
+        device_id: str,
+        access_token: str,
+        refresh_token: str,
+        oauth2_client_id: str,
+        oauth2_client_secret: str,
+    ):
         # HTTP request variables
-        self.call = f"enterprises/{project_id}/devices/{device_id}"
         self.end_point = "https://smartdevicemanagement.googleapis.com/v1"
+        self.call = f"enterprises/{project_id}/devices/{device_id}"
         self.url = f"{self.end_point}/{self.call}"
         self.headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {access_token}",
         }
 
+        # oauth2 variables
+        self.oauth2_url = "https://www.googleapis.com/oauth2/v4/token"
+        self.oauth2_data = {
+            "client_id": oauth2_client_id,
+            "client_secret": oauth2_client_secret,
+            "refresh_token": refresh_token,
+            "grant_type": "refresh_token",
+        }
+
         # Device variables
         self.mode = "OFF"  # {OFF, HEAT, MANUAL_ECO}
 
     def refresh_access_token(self) -> None:
-        # TODO: Implement the refreshing and saving of the access token
-        raise NotImplementedError
-    
+        """Refresh the access token"""
+        try:
+            r = requests.post(url=self.oauth2_url, data=self.oauth2_data)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise SystemExit("Error: Invalid OAuth2 credentials")
+
+        # Update access token in headers
+        response = r.json()
+        access_token = response["access_token"]
+        self.headers["Authorization"] = f"Bearer {access_token}"
+
+        # Update environment variable
+        dotenv_path = find_dotenv(".env")
+        set_key(dotenv_path, "ACCESS_TOKEN", access_token)
+
+        return None
+
     def fetch_device_traits(self) -> dict[str, any]:
+        """Fetch all available device traits"""
+        # TODO: Duplicated code, find a more elegant solution
         try:
             r = requests.get(url=self.url, headers=self.headers)
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            # self.refresh_access_token()
-            raise SystemExit("Error: Invalid authentication credentials")
+            # Aunauthorized error (401), attempt to refresh access token
+            self.refresh_access_token()
+            try:
+                r = requests.get(url=self.url, headers=self.headers)
+                r.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise SystemExit(e)
 
         # extracting data in json format
         response = r.json()
         traits = response["traits"]
         return traits
 
-    def set_device_traits(self, payload) -> None:
+    def set_device_traits(self, payload: dict[str, any]) -> None:
+        """Set device traits using the provided payload"""
+        # TODO: Duplicated code, find a more elegant solution
         try:
             r = requests.post(
                 url=f"{self.url}:executeCommand", headers=self.headers, json=payload
             )
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            # self.refresh_access_token()
-            raise SystemExit("Error: Invalid authentication credentials")
+            # Aunauthorized error (401), attempt to refresh access token
+            self.refresh_access_token()
+            try:
+                r = requests.post(
+                    url=f"{self.url}:executeCommand", headers=self.headers, json=payload
+                )
+                r.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise SystemExit(e)
         return None
 
     def get_ambient_humidity_percent(self, traits: dict[str, any] = None) -> int:
+        """Get the ambient humidity percentage"""
         # If traits are not provided, fetch them
         if traits is None:
             traits = self.fetch_device_traits()
+
         # Extract desired trait
         humidity_trait = traits["sdm.devices.traits.Humidity"]
         ambient_humidity_percent = humidity_trait["ambientHumidityPercent"]
         return ambient_humidity_percent
 
     def get_ambient_temperature_celsius(self, traits: dict[str, any] = None) -> float:
+        """Get the ambient temperature measured in celsius"""
         # If traits are not provided, fetch them
         if traits is None:
             traits = self.fetch_device_traits()
+
         # Extract desired trait
         temperature_trait = traits["sdm.devices.traits.Temperature"]
         ambient_temperature_celsius = temperature_trait["ambientTemperatureCelsius"]
         return ambient_temperature_celsius
 
     def get_temperature_setpoint(self, traits: dict[str, any] = None) -> float:
+        """Get the current temperature setpoint in celsius"""
         # If traits are not provided, fetch them
         if traits is None:
             traits = self.fetch_device_traits()
+
         # Extract desired trait
         setpoint_trait = traits["sdm.devices.traits.ThermostatTemperatureSetpoint"]
         temperature_setpoint = setpoint_trait["heatCelsius"]
         return temperature_setpoint
 
     def set_temperature_setpoint(self, value: float) -> None:
+        """Set the temperature setpoint in celsius"""
         payload = {
             "command": "sdm.devices.commands.ThermostatTemperatureSetpoint.SetHeat",
             "params": {"heatCelsius": value},
         }
         self.set_device_traits(payload)
-
-
-# def refresh_access_token():
-#     refresh_token = os.environ.get("REFRESH_TOKEN")
-#     print(refresh_token)
-#     # raise NotImplementedError
 
 
 @click.group()
@@ -104,6 +148,9 @@ def cli(ctx) -> None:
         project_id=os.environ["PROJECT_ID"],
         device_id=os.environ["DEVICE_ID"],
         access_token=os.environ["ACCESS_TOKEN"],
+        refresh_token=os.environ["REFRESH_TOKEN"],
+        oauth2_client_id=os.environ["OAUTH2_CLIENT_ID"],
+        oauth2_client_secret=os.environ["OAUTH2_CLIENT_SECRET"],
     )
 
     return None
--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
 # https://click.palletsprojects.com/en/8.1.x/setuptools/#setuptools-integration
+# TODO: include all the required packages
 
 setup(
-    name='NestPy',
-    version='0.1.0',
-    py_modules=['main'],
-    install_requires=[
-        'python-dotenv',
-        'click',
-    ],
+    name="NestPy",
+    version="0.1.0",
+    py_modules=["main"],
+    install_requires=["python-dotenv", "click", "requests"],
     entry_points={
-        'console_scripts': [
-            'nestpy = main:cli',
+        "console_scripts": [
+            "nestpy = main:cli",
         ],
     },
 )
